733da6adc6ff4ac89bc8d2c6a4e4960318fd6a84
Lino Prka <lino.prka@adnet.hr>
2021-05-28 10:41:35 +0200

    Added task descriptions

--- main.c
+++ main.c
@@ -114,7 +114,16 @@ int doTask1()
 int main()
 {
     printf("1. Napravite funkciju koja ce omoguciti mnozenje matrice cijelih brojeva i cijelog broja k te vratiti pokazivac na matricu umnoska. "
-           "U glavnom dijelu programa treba unijeti matricu reda nxm (2<=n<=10,2<=m<=10) i jedan cijeli broj k. Ispisati dobivenu matricu po redcima u formatu \"%%d\"\n");
+           "U glavnom dijelu programa treba unijeti matricu reda nxm (2<=n<=10,2<=m<=10) i jedan cijeli broj k. Ispisati dobivenu matricu po redcima u formatu \"%%d\"\n"
+           "2. Napisite C program koji ce omoguciti unos n<10 artikala u racun. Unos implementirati u datoteci functions.c prema deklaraciji strukutra u myheader.c datoteci, "
+           "pri cemu je primjer izvodenja programa, a ujedno i test case dan u komentaru u functions.c datoteci. Ispis racuna implementiran je u program.c datoteci, te ga "
+           "nije moguce mijenjati.\n"
+           "3. Napisite C program koji ucitava tocke i trokute iz datoteke model.txt. Program iz datoteke ucitava prvo broj tocaka n, zatim broj trokuta m, a zatim tocke i "
+           "trokute. Potrebno je dinamicki alocirati memoriju za unos tocaka i trokuta. Pronaci i ispisati opseg trokuta koji ima najveci opseg u formatu \"%%.2f\"\n"
+           "4. Napisite C program koji ce iz datoteke racun.txt ucitati n artikala u racun. Datoteka racun.txt je vec zadana, a sadrzi ime kupca, ime prodavaca, broj artikala "
+           "u racunu i podatke o svakom od artikala. Program treba ispisati ukupan iznos racuna na ekran u obliku: \"Ukupan iznos racuna: %%.2f\". Postavljen je oblik "
+           "struktura, primjetite da je unutar struktura svu memoriju za polja (bilo znakova bilo struktura) potrebno alocirati dinamicki. Kako je struktura zadana, na "
+           "osnovu tipova podataka clanova strukture i oblika podataka u ulaznoj datoteci mozete zakljuciti kako trebate upisati ulazne podatke.\n");
 
     int option;
     if (readInt("Unesite broj zadatka (1-4): ", 1, 4, &option) < 0)
